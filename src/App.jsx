70cb2c8e58d9e9215670d116a5cf62e9c55a794d
Intellcoder <chimeziejoshuac124@gmail.com>
2025-02-10 00:30:19 +0100

    resolved router issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./body/Header";
 import Body from "./body/Body";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./body/About";
 import AllCourses from "./body/AllCourses";
 import GetStarted from "./body/GetStarted";
@@ -12,19 +12,21 @@ import ErrorPage from "./body/components/ErrorPage";
 
 const App = () => {
   return (
-    <div className="m-1">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Body />} />
-        <Route path="about" element={<About />} />
-        <Route path="/courses" element={<AllCourses />} />
-        <Route path="/onboarding" element={<OnBoarding />} />
-        <Route path="/application" element={<GetStarted />} />
-        <Route path="*" element={<ErrorPage />} />
-        <Route path="/succesful" element={<SuccessCard />} />
-      </Routes>
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="p-2">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Body />} />
+          <Route path="about" element={<About />} />
+          <Route path="/courses" element={<AllCourses />} />
+          <Route path="/onboarding" element={<OnBoarding />} />
+          <Route path="/application" element={<GetStarted />} />
+          <Route path="*" element={<ErrorPage />} />
+          <Route path="/succesful" element={<SuccessCard />} />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 };
 
--- src/body/components/ErrorPage.jsx
+++ src/body/components/ErrorPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ErrorPage = () => {
   return (
-    <div className="bg-[purple] h-[50vh] flex justify-center items-center mt-3 mb-3">
+    <div className="bg-[purple] h-[50vh] flex justify-center items-center mt-3 mb-3 m-2">
       <h1 className="text-3xl text-white">404 PAGE NOT FOUND 😎😎</h1>
     </div>
   );
